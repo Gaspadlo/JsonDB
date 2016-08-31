641a483d4f1955235f916cedbcabcb804654ba66
Gaspadlo <Gaspadlo@gmail.com>
2016-08-31 12:36:06 +0200

    Add files via upload

--- JsonDB.php
+++ JsonDB.php
@@ -7,6 +7,8 @@
  * Last change: 05-06-2012
  * Version: 1.0b
  * by Manuel Strauss, Web: http://straussn.eu, E-Mail: StrZlee@gmx.net, Skype: StrZlee
+ *
+ * Trim Removed to make class work as intended
  */
 
 /*
@@ -37,89 +39,90 @@
 
 class JsonDB
 {
-    /**
-     * @var string
-     */
-    protected $path = './';
+	/**
+	 * @var string
+	 */
+	protected $path = './';
 
-    /**
-     * @var string
-     */
-    protected $file_ext = '.json';
+	/**
+	 * @var string
+	 */
+	protected $file_ext = '.json';
 
-    /**
-     * @var array
-     */
-    protected $tables = [];
+	/**
+	 * @var array
+	 */
+	protected $tables = [];
 
-    /**
-     * Construct the object and set the base path for databases.
-     *
-     * @param $path
-     * @throws Exception
-     */
-    public function __construct($path)
-    {
-        if (!is_dir($path)) {
-            throw new Exception("JsonDB Error: Database not found.");
-        }
+	/**
+	 * Construct the object and set the base path for databases.
+	 *
+	 * @param $path
+	 * @throws Exception
+	 */
+	public function __construct($path)
+	{
+		if (!is_dir($path)) {
+			throw new Exception("JsonDB Error: Database not found.");
+		}
 
-        $this->path = rtrim($path, '/');
-    }
+		//$this->path = rtrim($path, '/');
+		$this->path = $path;
+	}
 
-    /**
-     * Get table instance if exists or create new one.
-     *
-     * @param $table
-     * @param $create
-     * @return mixed
-     */
-    protected function getTableInstance($table, $create)
-    {
-        if (!isset($this->tables[$table])) {
-            $this->tables[$table] = new JsonTable($this->path . $table, $create);
-        }
+	/**
+	 * Get table instance if exists or create new one.
+	 *
+	 * @param $table
+	 * @param $create
+	 * @return mixed
+	 */
+	protected function getTableInstance($table, $create)
+	{
+		if (!isset($this->tables[$table])) {
+			$this->tables[$table] = new JsonTable($this->path . $table, $create);
+		}
 
-        return $this->tables[$table];
-    }
+		return $this->tables[$table];
+	}
 
-    /**
-     * Magic call for JsonTable class.
-     *
-     * @param $op
-     * @param $args
-     * @return mixed
-     * @throws Exception
-     */
-    public function __call($op, $args)
-    {
-        if ($args and method_exists('JsonTable', $op)) {
-            $table = $args[0] . $this->file_ext;
-            $create = false;
+	/**
+	 * Magic call for JsonTable class.
+	 *
+	 * @param $op
+	 * @param $args
+	 * @return mixed
+	 * @throws Exception
+	 */
+	public function __call($op, $args)
+	{
+		if ($args and method_exists('JsonTable', $op)) {
+			$table = $args[0] . $this->file_ext;
+			$create = false;
 
-            if ($op == 'createTable') {
-                return $this->getTableInstance($table, true);
-            } elseif ($op == 'insert' and isset($args[2]) and $args[2] === true) {
-                $create = true;
-            }
+			if ($op == 'createTable') {
+				return $this->getTableInstance($table, true);
+			} elseif ($op == 'insert' and isset($args[2]) and $args[2] === true) {
+				$create = true;
+			}
 
-            return $this->getTableInstance($table, $create)->$op($args);
-        } else {
-            throw new Exception("JsonDB Error: Unknown method or wrong arguments.");
-        }
-    }
+			return $this->getTableInstance($table, $create)->$op($args);
+		} else {
+			throw new Exception("JsonDB Error: Unknown method or wrong arguments.");
+		}
+	}
 
-    /**
-     * Set extension for json file.
-     *
-     * @param $file_ext
-     * @return $this
-     */
-    public function setExtension($file_ext)
-    {
-        $this->file_ext = $file_ext;
-        return $this;
-    }
+	/**
+	 * Set extension for json file.
+	 *
+	 * @param $file_ext
+	 * @return $this
+	 */
+	public function setExtension($file_ext)
+	{
+		$this->file_ext = $file_ext;
+		return $this;
+	}
 
 }
 ?>
